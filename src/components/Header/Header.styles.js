ca0aaff1daff36910097530953aea55944b0a8e2
HighonAces <srjnreddy33@gmail.com>
2022-01-14 16:47:45 -0600

    initial commit

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  padding: 4em;
+  background: #292826;
+`
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #F9D342;
+`
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Title, Wrapper} from "./Header.styles";
+
+const Header = () => (
+    <Wrapper>
+        <Title>
+            Hello, This is Srujan
+        </Title>
+    </Wrapper>
+)
+
+export default Header;
