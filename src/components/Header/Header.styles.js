3fa974b2ce0e5ded777ae3f081869cefec52ccd9
HighonAces <srjnreddy33@gmail.com>
2022-01-14 17:24:50 -0600

    added additional content

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -4,9 +4,14 @@ export const Wrapper = styled.section`
   padding: 4em;
   background: #292826;
 `
-
 export const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
+  font-family: 'Abel', sans-serif;
   color: #F9D342;
+`
+
+export const Content = styled.p`
+  color: #F9D342;
+  font-family: 'Abel', sans-serif;
 `
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {Title, Wrapper} from "./Header.styles";
+import {Content, Title, Wrapper} from "./Header.styles";
 
 const Header = () => (
     <Wrapper>
         <Title>
             Hello, This is Srujan
         </Title>
+        <Content>
+            I am a fullstack developer always interested in working on latest technologies.
+        </Content>
     </Wrapper>
 )
 
